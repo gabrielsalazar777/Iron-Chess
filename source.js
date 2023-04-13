869543814a93eaabff0b0e698839a8a05ac34042
Gabriel Salazar <gabriel.salazar777@gmail.com>
2023-04-13 11:52:05 -0400

    full game logic complete prior to adding buttons

--- source.js
+++ source.js
@@ -3,127 +3,96 @@
 let darkPawn1 = document.createElement("img");
 darkPawn1.id = "dark-pawn-1";
 darkPawn1.src = "images/dark_pawn.svg";
-
 let darkPawn2 = document.createElement("img");
 darkPawn2.id = "dark-pawn-2";
 darkPawn2.src = "images/dark_pawn.svg";
-
 let darkPawn3 = document.createElement("img");
 darkPawn3.id = "dark-pawn-3";
 darkPawn3.src = "images/dark_pawn.svg";
-
 let darkPawn4 = document.createElement("img");
 darkPawn4.id = "dark-pawn-4";
 darkPawn4.src = "images/dark_pawn.svg";
-
 let darkPawn5 = document.createElement("img");
 darkPawn5.id = "dark-pawn-5";
 darkPawn5.src = "images/dark_pawn.svg";
-
 let darkPawn6 = document.createElement("img");
 darkPawn6.id = "dark-pawn-6";
 darkPawn6.src = "images/dark_pawn.svg";
-
 let darkPawn7 = document.createElement("img");
 darkPawn7.id = "dark-pawn-7";
 darkPawn7.src = "images/dark_pawn.svg";
-
 let darkPawn8 = document.createElement("img");
 darkPawn8.id = "dark-pawn-8";
 darkPawn8.src = "images/dark_pawn.svg";
-
 let darkRook1 = document.createElement("img");
 darkRook1.id = "dark-rook-1";
 darkRook1.src = "images/dark_rook.svg";
-
 let darkRook2 = document.createElement("img");
 darkRook2.id = "dark-rook-2";
 darkRook2.src = "images/dark_rook.svg";
-
 let darkKnight1 = document.createElement("img");
 darkKnight1.id = "dark-knight-1";
 darkKnight1.src = "images/dark_knight.svg";
-
 let darkKnight2 = document.createElement("img");
 darkKnight2.id = "dark-knight-2";
 darkKnight2.src = "images/dark_knight.svg";
-
 let darkBishop1 = document.createElement("img");
 darkBishop1.id = "dark-bishop-1";
 darkBishop1.src = "images/dark_bishop.svg";
-
 let darkBishop2 = document.createElement("img");
 darkBishop2.id = "dark-bishop-2";
 darkBishop2.src = "images/dark_bishop.svg";
-
 let darkQueen = document.createElement("img");
 darkQueen.id = "dark-queen";
 darkQueen.src = "images/dark_queen.svg";
-
 let darkKing = document.createElement("img");
 darkKing.id = "dark-king";
 darkKing.src = "images/dark_king.svg";
-
 let lightPawn1 = document.createElement("img");
 lightPawn1.id = "light-pawn-1";
 lightPawn1.src = "images/light_pawn.svg";
-
 let lightPawn2 = document.createElement("img");
 lightPawn2.id = "light-pawn-2";
 lightPawn2.src = "images/light_pawn.svg";
-
 let lightPawn3 = document.createElement("img");
 lightPawn3.id = "light-pawn-3";
 lightPawn3.src = "images/light_pawn.svg";
-
 let lightPawn4 = document.createElement("img");
 lightPawn4.id = "light-pawn-4";
 lightPawn4.src = "images/light_pawn.svg";
-
 let lightPawn5 = document.createElement("img");
 lightPawn5.id = "light-pawn-5";
 lightPawn5.src = "images/light_pawn.svg";
-
 let lightPawn6 = document.createElement("img");
 lightPawn6.id = "light-pawn-6";
 lightPawn6.src = "images/light_pawn.svg";
-
 let lightPawn7 = document.createElement("img");
 lightPawn7.id = "light-pawn-7";
 lightPawn7.src = "images/light_pawn.svg";
-
 let lightPawn8 = document.createElement("img");
 lightPawn8.id = "light-pawn-8";
 lightPawn8.src = "images/light_pawn.svg";
-
 let lightRook1 = document.createElement("img");
 lightRook1.id = "light-rook-1";
 lightRook1.src = "images/light_rook.svg";
-
 let lightRook2 = document.createElement("img");
 lightRook2.id = "light-rook-2";
 lightRook2.src = "images/light_rook.svg";
-
 let lightKnight1 = document.createElement("img");
 lightKnight1.id = "light-knight-1";
 lightKnight1.src = "images/light_knight.svg";
-
 let lightKnight2 = document.createElement("img");
 lightKnight2.id = "light-knight-2";
 lightKnight2.src = "images/light_knight.svg";
-
 let lightBishop1 = document.createElement("img");
 lightBishop1.id = "light-bishop-1";
 lightBishop1.src = "images/light_bishop.svg";
-
 let lightBishop2 = document.createElement("img");
 lightBishop2.id = "light-bishop-2";
 lightBishop2.src = "images/light_bishop.svg";
-
 let lightQueen = document.createElement("img");
 lightQueen.id = "light-queen";
 lightQueen.src = "images/light_queen.svg";
-
 let lightKing = document.createElement("img");
 lightKing.id = "light-king";
 lightKing.src = "images/light_king.svg";
@@ -133,23 +102,7 @@ function setPiece(square, piece) {
   document.getElementById(square).appendChild(piece);
 }
 
-// tester Piece class, might not be used
-class Piece {
-  constructor() {
-    this.square;
-    this.piece;
-    this.pieceType;
-  }
-
-  classSetPiece(square, piece, pieceType) {
-    document.getElementById(square).appendChild(piece);
-    this.square = square;
-    this.piece = piece;
-    this.pieceType = pieceType;
-  }
-}
-
-//board array for use in converting div ID to coords
+//board array for use in converting div ID to array matrix coords
 let boardArray = [
   ["A8", "B8", "C8", "D8", "E8", "F8", "G8", "H8"],
   ["A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7"],
@@ -161,7 +114,7 @@ let boardArray = [
   ["A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1"],
 ];
 
-//piece array for use in path checking
+//piece-square occupation array for use in path checking
 let pieceArray = [
   ["T", "T", "T", "T", "T", "T", "T", "T"],
   ["T", "T", "T", "T", "T", "T", "T", "T"],
@@ -176,11 +129,9 @@ let pieceArray = [
 let playerTurn;
 
 //BOARD INIT HERE!
-let dPawn1 = new Piece();
 function initBoard() {
   //set pieces to appropriate div square at init
-  dPawn1.classSetPiece("A7", darkPawn1, "pawn");
-  // setPiece("A7", darkPawn1);
+  setPiece("A7", darkPawn1);
   setPiece("B7", darkPawn2);
   setPiece("C7", darkPawn3);
   setPiece("D7", darkPawn4);
@@ -346,12 +297,6 @@ function isCheckOrMate() {
         boardArray.forEach((row) => {
           row.forEach((coord) => {
             if (avoidFriendlyCheck(e.id, e.parentNode.id, coord)) {
-              console.log(
-                "Line 360: lightMate false at: e.id, e.parentNode.id, coord: ",
-                e.id,
-                e.parentNode.id,
-                coord
-              );
               lightMate = false;
             }
           });
@@ -378,30 +323,21 @@ function avoidFriendlyCheck(id, origin, destination) {
   coordinateConversion(origin, destination);
   currentImages = document.querySelectorAll("img");
 
-  //if dest coords lead to 0 run first syntax
-  //if dest coords lead to T, run second syntax
-
   //TEST start testing code here
+  //pass correct order of arguments to isLegalMove if destination square empty
   let type = document.getElementById(id).className;
   if (pieceArray[yCoordD][xCoordD] === "0") {
-    console.log(
-      "387: type, id, origin, destination",
-      type,
-      id,
-      origin,
-      destination
-    );
     if (!isLegalMove(type, id, origin, "anything", destination)) {
       yCoordO = yCoordOTemp;
       xCoordO = xCoordOTemp;
       yCoordD = yCoordDTemp;
       xCoordD = xCoordDTemp;
-      console.log("line 399: movement to 0 square illegal: ", destination);
       return 0;
     }
   }
+
+  //pass correct order of arguments to isLegalMove if destination square occupied
   if (pieceArray[yCoordD][xCoordD] === "T") {
-    console.log("line 403: if hit");
     let tempCoords = boardArray[yCoordD][xCoordD];
     let targetId = document.getElementById(destination).firstChild.id;
     if (!isLegalMove(type, id, origin, targetId, destination)) {
@@ -409,11 +345,6 @@ function avoidFriendlyCheck(id, origin, destination) {
       xCoordO = xCoordOTemp;
       yCoordD = yCoordDTemp;
       xCoordD = xCoordDTemp;
-      console.log(
-        "line 411: movement to T square illegal: ",
-        targetId,
-        destination
-      );
       return 0;
     }
   }
@@ -430,14 +361,6 @@ function avoidFriendlyCheck(id, origin, destination) {
       if (!e.parentNode.id.includes(destination)) {
         if (isLegalMove(e.className, e.id, e.parentNode.id, id, destination)) {
           pieceArray = JSON.parse(JSON.stringify(tempArray));
-          console.log(
-            "428: illegal move, king throws themselves in check: ",
-            e.className,
-            e.id,
-            e.parentNode.id,
-            id,
-            destination
-          );
           legalMove = false;
         }
       }
@@ -474,14 +397,6 @@ function avoidFriendlyCheck(id, origin, destination) {
           )
         ) {
           pieceArray = JSON.parse(JSON.stringify(tempArray));
-          console.log(
-            "line 447: illegal move on white piece: ",
-            e.className,
-            e.id,
-            e.parentNode.id,
-            id,
-            destination
-          );
           legalMove = false;
         }
       }
@@ -535,10 +450,10 @@ function isLegalMove(type, id, origin, destPieceId, destination) {
   // console.log("Origin info: ", type, id, yCoordO, xCoordO, origin);
   // console.log("Destination info: ", destPieceId, yCoordD, xCoordD, destination);
 
-  //handle move based on img class ('type')
+  //handle move based on img class (param named 'type')
   switch (type) {
     case "pawn":
-      //if any pawn moving diagonal 2 or more
+      //if any pawn moving diagonal 2+ xcoords
       if (Math.abs(xCoordD - xCoordO) >= 2) {
         return 0;
       }
@@ -561,7 +476,6 @@ function isLegalMove(type, id, origin, destPieceId, destination) {
           (xCoordD - xCoordO === 1 || xCoordD - xCoordO === -1) &&
           yCoordD - yCoordO === 1
         ) {
-          console.log("line 537: false");
           return 0;
         }
         //if moving two squares not on init, or moving 3+ squares
@@ -809,7 +723,6 @@ function isLegalMove(type, id, origin, destPieceId, destination) {
   }
 
   //friendly fire
-  console.log("line 818: ", destPieceId);
   if (destPieceId) {
     if (
       (id.includes("light") && destPieceId.includes("light")) ||
@@ -864,29 +777,17 @@ board.forEach((elem) => {
           g.target.id
         )
       ) {
-        // console.log("line 812:", pieceArray);
         pieceArray[yCoordO][xCoordO] = "0";
         pieceArray[yCoordD][xCoordD] = "T";
-        // console.log("Line 815: ", pieceArray);
         selectedPiece.parentNode.removeChild(selectedPiece);
         g.target.appendChild(selectedPiece);
         lightKingParentId = lightKing.parentNode.id;
         darkKingParentId = darkKing.parentNode.id;
         isCheckOrMate();
-        // console.log(
-        //   "final LK: ",
-        //   lightKingParentId,
-        //   " final DK: ",
-        //   darkKingParentId
-        // );
         if (playerTurn === "light") {
           playerTurn = "dark";
-          // console.log("line 829: ", pieceArray);
-          // console.log("playerTurn ended1 : light");
         } else if (playerTurn === "dark") {
           playerTurn = "light";
-          // console.log("line 813: ", pieceArray);
-          // console.log("playerTurn ended1 : dark");
         }
       }
     }
@@ -904,7 +805,6 @@ board.forEach((elem) => {
       ) {
         pieceArray[yCoordO][xCoordO] = "0";
         pieceArray[yCoordD][xCoordD] = "T";
-        // console.log("line 852: ", pieceArray);
         targetParentNode.removeChild(g.target);
         selectedPiece.parentNode.removeChild(selectedPiece);
         targetParentNode.appendChild(selectedPiece);
@@ -913,19 +813,9 @@ board.forEach((elem) => {
         isCheckOrMate();
         if (playerTurn === "light") {
           playerTurn = "dark";
-          // console.log("line 861: ", pieceArray);
-          // console.log("playerTurn ended2 : light");
         } else if (playerTurn === "dark") {
           playerTurn = "light";
-          // console.log("line 865: ", pieceArray);
-          // console.log("playerTurn ended2 : dark");
         }
-        // console.log(
-        //   "final LK: ",
-        //   lightKingParentId,
-        //   " final DK: ",
-        //   darkKingParentId
-        // );
       }
     }
   });
